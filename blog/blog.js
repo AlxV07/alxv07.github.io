d78a1c544ebaddb5f9769eb1330477c472fdfb65
AlxV07 <alexander.kai.chen@gmail.com>
2026-03-21 21:33:53 -0500

    feat(blogpost)+clean: removed unused script.js, updated google analytics script, drsef blog post

--- blog/blog.js
+++ blog/blog.js
@@ -52,4 +52,13 @@ document.addEventListener('DOMContentLoaded', () => {
     if (themeToggle) {
         themeToggle.addEventListener('click', toggleTheme);
     }
+
+    const gaScript = document.createElement('script');
+    gaScript.async = true;
+    gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-J8RSL65KCL';
+    document.head.appendChild(gaScript);
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-J8RSL65KCL');
 });
